aede70fdfc9e1039b012f7d2905684b987bbd197
galk <gal.kaplon@mobileye.com>
2022-07-22 11:20:06 +0300

    mend

--- scripts/imagenet_4_gpu.sh
+++ scripts/imagenet_4_gpu.sh
@@ -3,8 +3,8 @@
 MY_RUN=$RANDOM$RANDOM
 OTHER_PARAMS=${@:1}
 
-#WANDB_API_KEY=641959d1c0dbfc348e2e0b75279abe93425c6ec7 python ./run.py -m \
-HYDRA_FULL_ERROR=1 WANDB_MODE=disabled python ./run.py -m \
+# WANDB_API_KEY=641959d1c0dbfc348e2e0b75279abe93425c6ec7 python ./run.py -m \
+WANDB_API_KEY=641959d1c0dbfc348e2e0b75279abe93425c6ec7 python ./run.py -m \
   run_id=$MY_RUN \
   logger.tags='[imagenet-one-gpu]' \
   datamodule.dataset=imagenet \
